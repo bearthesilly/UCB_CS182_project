db17686f73ebe38c02a023df5a9244f1a79f219c
jason <jason_lee@berkeley.edu>
2025-12-08 23:33:53 +0000

    fixed run_exp.sh

--- difficulty_experiments/run_experiments.sh
+++ difficulty_experiments/run_experiments.sh
@@ -2,6 +2,7 @@ source .venv/bin/activate && python difficulty_experiments/difficulty_mixing.py 
 source .venv/bin/activate && python difficulty_experiments/difficulty_mixing.py --selection-method "random"
 source .venv/bin/activate && python difficulty_experiments/difficulty_mixing.py --selection-method "difficulty_easy"
 source .venv/bin/activate && python difficulty_experiments/difficulty_mixing.py --selection-method "loss_ascending"
+source .venv/bin/activate && python difficulty_experiments/difficulty_mixing.py --selection-method "loss_descending"
 
 # Then run the following script, replacing the placeholder with the output files
 # source .venv/bin/activate && python difficulty_mixing/plot_forgetting_history.py -o test_output.png # <put output files here> 2>&1
